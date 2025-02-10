9d71fe3570a9435be6e298ef8ddba9a52bf77074
Chris Beck <beck.ct@gmail.com>
2025-02-09 17:41:28 -0700

    don't use frozen, use locked

--- build_rust.sh
+++ build_rust.sh
@@ -11,9 +11,9 @@ pushd mobilecoin
 
 export SGX_MODE=HW IAS_MODE=PROD CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css
 
-cargo build --release -p mc-mobilecoind --features mc-mobilecoind/bypass-ip-check --frozen
+cargo build --release -p mc-mobilecoind --features mc-mobilecoind/bypass-ip-check --locked
 popd
 
-cargo build --release --frozen
+cargo build --release --locked
 
 popd
